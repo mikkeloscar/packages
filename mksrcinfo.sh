80c8a4b4c26d2394f0b0bd0a86f36be1e50dd5a7
Mikkel Oscar Lyderik Larsen <mikkel.larsen@zalando.de>
2018-01-05 18:26:33 +0100

    Update mksrcinfo script to use makepkg --printsrcinfo

--- mksrcinfo.sh
+++ mksrcinfo.sh
@@ -6,7 +6,7 @@ for pkg in $pwd/*; do
     if [ -d "${pkg}" ]; then
         echo "Updating .SRCINFO for ${pkg#$pwd/}"
         cd ${pkg}
-        mksrcinfo
+        makepkg --printsrcinfo > .SRCINFO
         # strip line with creation date
         gawk -i inplace 'NR != 2' .SRCINFO
         cd ..
